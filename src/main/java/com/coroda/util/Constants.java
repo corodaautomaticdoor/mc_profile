5b1d05350bf2854bd6b45c2f0e9f52fdf6b4fa1c
Lucero_Ampudia <luamtri24@gmail.com>
2020-08-08 01:30:05 -0500

    feat : implementa logica de la clase constants

--- src/main/java/com/coroda/util/Constants.java
+++ src/main/java/com/coroda/util/Constants.java
@@ -1,5 +1,22 @@
 package com.coroda.util;
 
 public class Constants {
- 
+    public final static String MAIN_PATH = "/person";
+    public final static String ID = "/{id}";
+
+    public final static String SUCCESS = "SUCCESS";
+    public final static String ERROR = "ERROR";
+
+    public final static String SAVE_VALUE="Metodo a traves del cual se envia la informacion del Perfil que  sera registrada dentro de la base de datos";
+    public final static String SAVE_NOTE="Para el registro del Perfil , sera necesario el llenado de todo los campos , a excepcion de los Id´s los cuales seran generados automaticamente";
+    public final static String DELETE_ID_VALUE="Metodo a traves del cual se Elimina la informacion del Perfil mediante su Id ";
+    public final static String DELETE_ID_NOTE="Para eliminar los  datos del Perfil , sera necesario enviar el Id del Perfil ";
+    public final static String UPDATE_ID_VALUE="Metodo a traves del cual se Actualiza la informacion del Perfil mediante su Id ";
+    public final static String UPDATE_ID_NOTE="Para Actualizar los  datos de un Perfil , sera necesario enviar todo el registro con los datos ya actualizados junto a sus Id`s ";
+    public final static String GET_ID_VALUE="Metodo a traves del cual se Obtiene la informacion del Perfil mediante su Id ";
+    public final static String GET_ID_NOTE="Para la obtencion de datos de un Perfil , sera necesario enviar el Id del Producto a consultar";
+
+    public final static String GETDATA_VALUE="Metodo a traves del cual se Obtiene la informacion del Perfil ";
+    public final static String GETDATA_NOTE="Para la obtencion de datos del Perfil ";
+
 }
