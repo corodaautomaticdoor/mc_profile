405cf7ca5d261169861df516e6616b381dd9dcb6
Lucero_Ampudia <luamtri24@gmail.com>
2020-08-08 01:11:03 -0500

    feat : implementa logica de la capa service

--- src/main/java/com/coroda/service/PersonService.java
+++ src/main/java/com/coroda/service/PersonService.java
@@ -1,5 +1,15 @@
 package com.coroda.service;
 
+import com.coroda.dto.request.PersonRequest;
+import com.coroda.dto.response.PersonResponse;
+import io.reactivex.Completable;
+import io.reactivex.Observable;
+import io.reactivex.Single;
+
 public interface PersonService {
-	
+    Completable save(PersonRequest model);
+    Observable<PersonResponse> findAll();
+    Single<PersonResponse> getById(Long idPerson);
+    Completable delete(Long idPerson);
+    Completable update(PersonRequest model);
 }
--- src/main/java/com/coroda/service/implement/PersonServiceImplement.java
+++ src/main/java/com/coroda/service/implement/PersonServiceImplement.java
@@ -1,7 +1,43 @@
 package com.coroda.service.implement;
 
+import com.coroda.dao.PersonDao;
+import com.coroda.dto.request.PersonRequest;
+import com.coroda.dto.response.PersonResponse;
 import com.coroda.service.PersonService;
+import io.reactivex.Completable;
+import io.reactivex.Observable;
+import io.reactivex.Single;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class PersonServiceImplement implements PersonService {
 
+    @Autowired
+    private PersonDao personDao;
+
+    @Override
+    public Completable save(PersonRequest model) {
+        return personDao.save(model);
+    }
+
+    @Override
+    public Observable<PersonResponse> findAll() {
+        return personDao.findAll();
+    }
+
+    @Override
+    public Single<PersonResponse> getById(Long idPerson) {
+        return personDao.getById(idPerson);
+    }
+
+    @Override
+    public Completable delete(Long idPerson) {
+        return personDao.delete(idPerson);
+    }
+
+    @Override
+    public Completable update(PersonRequest model) {
+        return personDao.update(model);
+    }
 }
