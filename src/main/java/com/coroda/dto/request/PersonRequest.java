9aae8f977328b9474d0450db622ef6c59ebfba50
garychavez <52689437+garychavez@users.noreply.github.com>
2020-08-08 00:49:44 -0500

    feat : implementa logica de la capa dto

--- src/main/java/com/coroda/dto/request/PersonRequest.java
+++ src/main/java/com/coroda/dto/request/PersonRequest.java
@@ -1,5 +1,45 @@
 package com.coroda.dto.request;
 
+import com.coroda.entity.TypeDocument;
+import com.coroda.entity.TypePerson;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@ApiModel("Model Person")
+@AllArgsConstructor
 public class PersonRequest {
+
+   @ApiModelProperty("id")
+   private long idPerson;
+
+   @ApiModelProperty("typeDocument")
+   private TypeDocument typeDocument;
+
+   @ApiModelProperty("typePerson")
+   private TypePerson typePerson;
+
+   @ApiModelProperty("name")
+   private String name;
+
+   @ApiModelProperty("lastName1")
+   private String lastName1;
+
+   @ApiModelProperty("lastName2")
+   private String lastName2;
+
+   @ApiModelProperty("socialReason")
+   private String socialReason;
+
+   @ApiModelProperty("address")
+   private String address;
+
+   @ApiModelProperty("email")
+   private String email;
+
+   @ApiModelProperty("phone")
+   private long phone;
 
    }
--- src/main/java/com/coroda/dto/response/PersonResponse.java
+++ src/main/java/com/coroda/dto/response/PersonResponse.java
@@ -1,5 +1,34 @@
 package com.coroda.dto.response;
 
+import com.coroda.entity.TypeDocument;
+import com.coroda.entity.TypePerson;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class PersonResponse {
+
+    private long idPerson;
+
+    private TypeDocument typeDocument;
+
+    private TypePerson typePerson;
+
+    private String name;
+
+    private String lastName1;
+
+    private String lastName2;
+
+    private String socialReason;
+
+    private String address;
+
+    private String email;
+
+    private long phone;
 
 }
