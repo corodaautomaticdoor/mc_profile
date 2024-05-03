3f10911f2d229d416a5d30a645bea1e30da65e8a
Ampudia Trigoso <lampudia@indracompany.com>
2024-05-02 22:37:51 -0500

    feat: local configuration implementation

---
+++ src/main/resources/data.sql
@@ -0,0 +1,11 @@
+INSERT INTO person(id_person,type_document,type_person,number_document
+,name,last_name1,last_name2,social_reason,address,email,phone)
+VALUES(1,0,0,71723707,'Gary','Chavez','Jimenez','GALU','Av gasoducto','garychavezj@gmail.com', 955163628);
+
+INSERT INTO person(id_person,type_document,type_person,number_document
+,name,last_name1,last_name2,social_reason,address,email,phone)
+VALUES(2,0,0,74593040,'Lucero','Ampudia','Trigoso de Chavez','GALU','Av gasoducto','luamtri24@gmail.com', 960412212);
+
+INSERT INTO person(id_person,type_document,type_person,number_document
+,name,last_name1,last_name2,social_reason,address,email,phone)
+VALUES(3,0,0,93766225,'Itzel Massiel','Chavez','Ampudia','GALU','Av gasoducto','itzel@gmail.com', 960412212);
